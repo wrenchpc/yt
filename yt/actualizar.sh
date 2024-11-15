d121264f7c3566efa8452b19d0d194e4addacbff
Wrench <dedmause000@gmail.com>
2024-11-15 11:38:31 -0300

    Add files via upload

---
+++ yt/actualizar.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Actualiza automaticamente desde el repositorio de github
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+REPO_URL="https://github.com/Wworn/yt.git"
+
+TARGET_FOLDER="yt"
+
+TEMP_DIR=$(mktemp -d)
+
+git init "$TEMP_DIR"
+
+cd "$TEMP_DIR"
+
+git remote add origin "$REPO_URL"
+
+git config core.sparseCheckout true
+
+echo "$TARGET_FOLDER" >> .git/info/sparse-checkout
+
+git pull origin main
+
+if [ $? -eq 0 ]; then
+    echo "Carpeta '$TARGET_FOLDER' clonada correctamente."
+
+    cp -r "$TEMP_DIR/$TARGET_FOLDER"/* "$SCRIPT_DIR"
+
+    echo "Archivos actualizados en la carpeta del script."
+else
+    echo "Error al clonar la carpeta."
+    exit 1
+fi
+
+rm -rf "$TEMP_DIR"
+
