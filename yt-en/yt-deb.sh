ff3fbf73d1aa0bca45cbd36e05417274ce7b4e2b
Wrench <dedmause000@gmail.com>
2024-11-18 21:40:38 -0300

    Add files via upload

---
+++ yt-en/yt-deb.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Script creado por wr3nch
+
+clear
+
+
+function banner() {
+    clear
+    cat << "EOF"
+       .---.
+      /     \
+      \.@-@./
+      /`\_/`\
+     //  _  \\
+    | \     )|_
+   /`\_`>  <_/ \
+   \__/'---'\__/
+
+¡¡¡WELCOME TO YT.SH!!!
+
+Press "ENTER" to continue...
+EOF
+    read -p ""
+echo ""   
+}
+
+banner
+
+confirm_install() {
+    echo "************************************"
+    echo "*  Dependences are not installed   *"
+    echo "*  Do you want to install them?    *"
+    echo "************************************"
+    read -p "Write 'yes' to install: " confirm
+    if [[ "$confirm" == "yes" ]]; then
+
+	sudo apt install snap -y        
+        sudo apt install mpv -y
+        sudo snap install yt-dlp -y
+	sudo apt install python -y
+        sudo apt install python-pip -y
+
+    else
+        echo "The instalation was canceled."
+        exit 1
+    fi
+}
+
+if ! command -v yt-dlp &> /dev/null || ! command -v mpv &> /dev/null; then
+    confirm_install
+fi
+
+clear
+
+echo "Select an option:"
+echo "1) Listen to Music (1)"
+echo "2) Download Music (2)"
+echo "3) Download Playlist (3)"
+echo "4) Download Video (4)"
+echo "5) EXIT (5)"
+
+read -p "Set a number (1-5): " option
+
+case $option in
+    1)
+        echo "Executing Script 1..."
+        sh Scripts/ytplay.sh
+        ;;
+    2)
+        echo "Executing Script 2..."
+        sh Scripts/yt-mp3-download.sh
+        ;;
+    3)
+        echo "Executing Script 3..."
+        sh Scripts/yt-playlist.sh
+        ;;
+    4)
+        echo "Executing Script 4..."
+        sh Scripts/ytvideod.sh
+        ;;
+    5) 
+	exit
+	;;
+    *)
+        ./yt-mint.sh
+	;;
+esac
+
---
+++ yt/yt-deb.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Script creado por wr3nch
+
+clear
+
+
+function banner() {
+    clear
+    cat << "EOF"
+       .---.
+      /     \
+      \.@-@./
+      /`\_/`\
+     //  _  \\
+    | \     )|_
+   /`\_`>  <_/ \
+   \__/'---'\__/
+
+¡¡¡BIENVENIDO AL YT.SH!!!
+
+Presiona "ENTER" para continuar...
+EOF
+    read -p ""
+echo ""   
+}
+
+banner
+
+confirm_install() {
+    echo "************************************"
+    echo "*  Componentes necesarios no       *"
+    echo "*  instalados. Desea instalarlos?  *"
+    echo "************************************"
+    read -p "Escriba 'si' para instalar: " confirm
+    if [[ "$confirm" == "si" ]]; then
+
+	sudo apt install snap -y        
+        sudo apt install mpv -y
+	sudo snap install yt-dlp -y
+        sudo apt install python -y
+        sudo apt install python-pip -y
+
+    else
+        echo "La instalación fue cancelada."
+        exit 1
+    fi
+}
+
+if ! command -v yt-dlp &> /dev/null || ! command -v mpv &> /dev/null; then
+    confirm_install
+fi
+
+clear
+
+echo "Seleccione la opción:"
+echo "1) Escuchar música (1)"
+echo "2) Descargar música (2)"
+echo "3) Descargar playlist (3)"
+echo "4) Descargar video (4)"
+echo "5) SALIR (5)"
+
+read -p "Ingrese el número (1-5): " option
+
+case $option in
+    1)
+        echo "Ejecutando Script 1..."
+        sh Scripts/ytplay.sh
+        ;;
+    2)
+        echo "Ejecutando Script 2..."
+        sh Scripts/yt-mp3-download.sh
+        ;;
+    3)
+        echo "Ejecutando Script 3..."
+        sh Scripts/yt-playlist.sh
+        ;;
+    4)
+        echo "Ejecutando Script 4..."
+        sh Scripts/ytvideod.sh
+        ;;
+    5)
+	exit
+	;;
+    *)
+        ./yt-mint.sh
+        ;;
+esac
+
