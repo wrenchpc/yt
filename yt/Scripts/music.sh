4e7e25a10ba31aa2160ad71c61f1d46cde95219b
Wrench <dedmause000@gmail.com>
2024-12-06 02:46:50 -0300

    Add files via upload

--- yt/Scripts/music.sh
+++ yt/Scripts/music.sh
@@ -4,29 +4,30 @@ clear
 
 echo "¿Que te gustaría hacer?:"
 echo "1) Escuchar música"
-echo "2) Descargar Música"
-echo "3) Escuchar Playlist"
-echo "4) Descargar Playlist"
-echo "5) Volver"
+echo "2) Escuchar música descargada"
+echo "3) Descargar música"
+echo "4) Escuchar una playlist"
+echo "5) Descargar una playlist"
 
-read -p "Elige una opción (1 o 4): " choice
+read -p "Elige una opción (1 o 5): " choice
 
 case $choice in
     1)
         sh Scripts/ytplay.sh
         ;;
+
     2)
-        sh Scripts/yt-mp3-download.sh
+        sh Scripts/musicplaylocal.sh
         ;;
     3)
-        sh Scripts/playlistplay.sh
+        sh Scripts/yt-mp3-download.sh
         ;;
     4)
-        sh Scripts/yt-playlist.sh
+        sh Scripts/playlistplay.sh
         ;;
     5)
-	sh yt.sh
-	;;
+        sh Scripts/yt-playlist.sh
+        ;;
     *)
         echo "Opción inválida. Saliendo..."
         exit 1
