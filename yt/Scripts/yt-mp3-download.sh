3b71a134d957d89a25bd8342b827203d6356f92c
Wrench <dedmause000@gmail.com>
2024-12-06 14:55:03 -0300

    Update yt-mp3-download.sh

--- yt/Scripts/yt-mp3-download.sh
+++ yt/Scripts/yt-mp3-download.sh
@@ -2,20 +2,6 @@
 # Script creado por wr3nch
 
 clear
-desktop_path="$HOME/YT"
-
-yt_folder="$desktop_path/Music"
-if [ ! -d "$yt_folder" ]; then
-    echo "Creando la carpeta 'YT/Music' en el usuario..."
-    mkdir -p "$yt_folder"
-else
-    echo ""
-fi
-if ! command -v yt-dlp &> /dev/null; then
-    echo "Error: yt-dlp no está instalado. Instálalo e intenta de nuevo."
-    exit 1
-fi
-
 read -p "Introduce el término de búsqueda (canción, artista, etc.): " search_term
 
 echo "Buscando en YouTube..."
