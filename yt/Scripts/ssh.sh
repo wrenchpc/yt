559a259b0d65b5d9ab617875d711d92acc089eba
Wrench <dedmause000@gmail.com>
2024-12-07 02:21:36 -0300

    Add files via upload

--- yt/Scripts/ssh.sh
+++ yt/Scripts/ssh.sh
@@ -1,38 +1,41 @@
 clear
-echo "¿Que desea compartir?"
+echo "¿Qué desea compartir?"
 
 echo "1) Carpeta de Música"
 echo "2) Carpeta de Playlists"
 echo "3) Carpeta de Videos"
 
-read -p "ingrese el número del (1-3): " option
+read -p "Ingrese el número del (1-3): " option
+
+# Generar un puerto dinámico entre 8000 y 9000
+PORT=$(shuf -i 8000-9000 -n 1)
 
 case $option in
-
-   1)
-	clear
-	python -m http.server --directory $HOME/YT/Music 8080 &
-	SERVER_PID=$! &
-	trap "echo 'Cerrando el servidor...'; kill $SERVER_PID" SIGINT &
-	wait &
-	;;
-   2)
-	clear
-	python -m http.server --directory $HOME/YT/Playlist 8080 &
-	SERVER_PID=$! &
-	trap "echo 'Cerrando el servidor...'; kill $SERVER_PID" SIGINT &
-	wait &
-	;;
-   3)
-	clear
-	python -m http.server --directory $HOME/YT/Music 8080 &
-	SERVER_PID=$! &
-	trap "echo 'Cerrando el servidor...'; kill $SERVER_PID" SIGINT &
-	wait &
-	;;
-   *)
-	./yt.sh
-	;;
+    1)
+        clear
+        python -m http.server --directory $HOME/YT/Music $PORT &
+        SERVER_PID=$! &
+        trap "echo 'Cerrando el servidor...'; kill $SERVER_PID" SIGINT &
+        wait &
+        ;;
+    2)
+        clear
+        python -m http.server --directory $HOME/YT/Playlist $PORT &
+        SERVER_PID=$! &
+        trap "echo 'Cerrando el servidor...'; kill $SERVER_PID" SIGINT &
+        wait &
+        ;;
+    3)
+        clear
+        python -m http.server --directory $HOME/YT/Videos $PORT &
+        SERVER_PID=$! &
+        trap "echo 'Cerrando el servidor...'; kill $SERVER_PID" SIGINT &
+        wait &
+        ;;
+    *)
+        ./yt.sh
+        ;;
 esac
 
-ssh -R 80:localhost:8080 nokey@localhost.run
+ssh -R 80:localhost:$PORT nokey@localhost.run
+
