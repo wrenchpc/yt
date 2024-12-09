76b4ca305a8b28c3bfc1afb6178a5d620dd1eef5
Wrench <dedmause000@gmail.com>
2024-12-09 01:32:41 -0300

    Update linux_mint_snap.sh

--- linux_mint_snap.sh
+++ linux_mint_snap.sh
@@ -1,3 +1,6 @@
+# Esto ejecutalo en la versión de Linux mint, Dará la opción de instalar snaps para tener la versión
+# Más actualizada de yt-dlp y sin problemas en su versión de debian.
+
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update
 sudo apt install snapd
